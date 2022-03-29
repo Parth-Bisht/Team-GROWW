8cd1fdb604107e328d4acb593e4c09dbf7c65183
Debobrota Haldar <99647439+iamdebobrota@users.noreply.github.com>
2022-03-29 16:36:53 +0530

    s

--- JS/usStock.js
+++ JS/usStock.js
@@ -1,10 +1,15 @@
 
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 let clickToShowFQS = document.getElementsByClassName("clickToShowFQS")
 
 let hiddenText = document.getElementsByClassName("hiddenText")
 
 let count = 0;
+<<<<<<< Updated upstream
 clickToShowArr.forEach((c, i) => {
     c.onclick = () => {
        
@@ -14,3 +19,31 @@ clickToShowArr.forEach((c, i) => {
     }
 
 })
+=======
+
+let hiddenTextArr = Array.from(hiddenText)
+let clickToShowArr = Array.from(clickToShowFQS)
+clickToShowArr.forEach((c, i) => {
+    c.onclick = () => {
+        count++;
+        if (count % 2 !== 0) {
+            hiddenText[i].style.display = "block"
+        } else {
+            hiddenText[i].style.display = "none"
+        }
+        //  hiddenText[i-1].style.display="none"
+        //  hiddenText[i+1].style.display="none"
+        //  hiddenText[i+2].style.display="none"
+        //  hiddenText[i-2].style.display="none"
+    }
+
+})
+
+
+
+// let clickToShowFQS = document.getElementsByClassName("clickToShowFQS")
+// clickToShowFQS.addEventListener("click",()=>{
+// document.getElementsByClassName("hiddenText").style.display="block"
+    
+// })
+>>>>>>> Stashed changes
